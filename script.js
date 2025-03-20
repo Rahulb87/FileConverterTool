b5bd1370fe6012a947a08fa048df83d6272bc196
Rahulb87 <63098575+Rahulb87@users.noreply.github.com>
2025-03-20 14:46:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ document.getElementById('compress-btn').addEventListener('click', function() {
     const compressionLevel = parseFloat(document.getElementById('compression-level').value);
     const imageContainer = document.getElementById('output-image');
     const downloadLink = document.getElementById('download-link');
-
+    downloadLink.style.display = 'none';
+    
     if (fileInput.files && fileInput.files[0]) {
         const reader = new FileReader();
 
@@ -20,7 +21,7 @@ document.getElementById('compress-btn').addEventListener('click', function() {
                 canvas.height = img.height;
 
                 ctx.drawImage(img, 0, 0);
-
+                downloadLink.style.display = 'block';
                 canvas.toBlob(function(blob) {
                     const compressedImageUrl = URL.createObjectURL(blob);
                     imageContainer.src = compressedImageUrl;
